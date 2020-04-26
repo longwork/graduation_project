4db64a4bfd95b26dcfd250ae6f74f025bb963187
longwork <1749560162@qq.com>
2020-04-26 18:04:44 +0800

    2020-04-26-18 commit

--- graduation-design-end/src/test/java/wimi/student/GraduationDesignEndApplicationTests.java
+++ graduation-design-end/src/test/java/wimi/student/GraduationDesignEndApplicationTests.java
@@ -1,16 +1,27 @@
 package wimi.student;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GraduationDesignEndApplicationTests {
 
+    @Resource
+    DataSource dataSource;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Assert.assertNull(connection);
     }
 
 }
